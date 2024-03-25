9bca9b952889bd2cf3053c9e251441644cfc0277
AravindDasarathy <aravind.dasarat@gmail.com>
2024-03-25 00:58:39 -0400

    rendering skins with pagination

--- server.js
+++ server.js
@@ -25,5 +25,26 @@ app.get('/weapons', async (req, res) => {
   res.render('pages/weapons', { weapons: weaponsList })
 });
 
+app.get('/skins', async (req, res) => {
+  const weaponName = req.query.weapon_name;
+  const weapons = await axios.get('https://valorant-api.com/v1/weapons');
+  const weapon = weapons.data.data.find(weapon => weapon.displayName.toLowerCase() === weaponName.toLowerCase());
+  const skinsToExclude = [`Standard ${weaponName}`, 'Random Favorite Skin'];
+  const filteredSkins = weapon.skins.filter(skin => !skinsToExclude.includes(skin.displayName));
+
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = 12;
+  const offset = (page - 1) * pageSize;
+  const paginatedSkins = filteredSkins.slice(offset, offset + pageSize);
+  const totalPages = Math.ceil(filteredSkins.length / pageSize);
+
+  res.render('pages/skins', {
+    weapon_name: weaponName,
+    currentPage: page,
+    totalPages: totalPages,
+    skins: paginatedSkins
+  });
+});
+
 app.listen(PORT);
 console.log(`Server is listening on port ${PORT}`);
