bf87f188362f7116dc9c4d47ff259c560211273b
AravindDasarathy <aravind.dasarat@gmail.com>
2024-03-25 00:58:25 -0400

    providing click functionality to weapon cards

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
-$('#viewWeaponsButton').click(function() {
-  window.location.href = '/weapons';
+$(document).ready(function () {
+  $('#viewWeaponsButton').click(function () {
+    window.location.href = '/weapons';
+  });
+
+  $('.weapon-card').click(function () {
+    const weaponName = $(this).data('weapon-name');
+
+    window.location.href = `/skins?weapon_name=${weaponName}`;
+  });
 });
